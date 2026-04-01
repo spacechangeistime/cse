17d8c600e8b161c353d91fbd36057f928e4f8b52
Vipin Kumar <vipinkumar41@gmail.com>
2026-04-02 01:07:07 +0530

    c: heapsort algorithm

--- c/algorithms/heap-sort.c
+++ c/algorithms/heap-sort.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-#define MAX_SIZE 100
-
-int* getList(const char* msg, int* n) {
-	int* array = (int*)malloc(MAX_SIZE*sizeof(int));
-	printf("Enter the integer elemets of %s array(q to stop):\n", msg);
-	int i = 0;
-	while(i < MAX_SIZE && scanf("%d", &array[i]) == 1)
-		i++;
-	*n = i;
+int* genArray(int* n) {
+	*n = rand()%10+3;
+	int* array = (int*)malloc(*n*sizeof(int));
+	for(int i = 0; i < *n; i++)
+		array[i] = rand() % 100;
 	return array;
 }
-void printList(int* arr, int n) {
+void printArray(int* arr, int n, const char* msg) {
+	printf("%s\n", msg);
 	for(int i = 0; i < n; i++)
 		printf("%d ", arr[i]);
 	printf("\n");
 }
-
 void swap(int* arr, int i, int j) {
 	int t = arr[i];
 	arr[i] = arr[j];
@@ -36,23 +33,22 @@ void max_heapify(int* arr, int i, int n) {
 		max_heapify(arr, largest, n);
 	}
 }
-
-int* heap_sort(int* arr, int n) {
+void heap_sort(int* arr, int n) {
 	for(int i = n/2 - 1; i >= 0; i--)
 		max_heapify(arr, i, n);
 	for(int i = n - 1; i > 0; i--) {
 		swap(arr, 0, i);
 		max_heapify(arr, 0, i);
 	}
-	return arr;
 }
 
 int main() {
 	int n = 0;
-	int* array = getList("", &n);
-	printList(array, n);
-	int* sorted = heap_sort(array, n);
-	printList(sorted, n);
+	srand(time(NULL));
+	int* array = genArray(&n);
+	printArray(array, n, "unsorted:");
+	heap_sort(array, n);
+	printArray(array, n, "sorted:");
 	free(array);
 	return 0;
 }
